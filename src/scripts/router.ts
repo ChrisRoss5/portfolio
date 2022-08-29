5430c833fdd1ba485d90006bc65977f763824e4c
Kristijan Rosandić <kristijan.ros@gmail.com>
2022-08-29 18:28:17 +0200

    removed dynamic imports

--- src/scripts/router.ts
+++ src/scripts/router.ts
@@ -1,16 +1,20 @@
 import AboutVue from "@/views/About.vue";
-import Projects from "@/views/Projects.vue";
-const AccomplishmentsVue = () => import("@/views/about-me/Accomplishments.vue");
+import ProjectsVue from "@/views/Projects.vue";
+/* const AccomplishmentsVue = () => import("@/views/about-me/Accomplishments.vue");
 const DocumentsVue = () => import("@/views/about-me/Documents.vue");
 const EducationVue = () => import("@/views/about-me/Education.vue");
-const ExperienceVue = () => import("@/views/about-me/Experience.vue");
+const ExperienceVue = () => import("@/views/about-me/Experience.vue"); */
+import AccomplishmentsVue from "@/views/about-me/Accomplishments.vue";
+import DocumentsVue from "@/views/about-me/Documents.vue";
+import EducationVue from "@/views/about-me/Education.vue";
+import ExperienceVue from "@/views/about-me/Experience.vue";
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const component = {};
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/projects",
-    component: Projects,
+    component: ProjectsVue,
     children: [
       { path: "extensions", component },
       { path: "themes", component },
